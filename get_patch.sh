b69b381f0a5089659142d12efb19513224b311f4
Sherlock Wang <wangzhou.bry@gmail.com>
2018-03-16 01:40:26 +0800

    ...
    Signed-off-by: Sherlock Wang <wangzhou.bry@gmail.com>

--- get_patch.sh
+++ get_patch.sh
@@ -5,9 +5,12 @@
 #
 #
 
+BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+echo $BRANCH
+
 git log --oneline -- ./ > /tmp/patch
 
-last_commit=`grep $1 -n /tmp/patch | cut -d : -f 1`
+last_commit=`grep "$1" -n /tmp/patch | cut -d : -f 1`
 let last_commit_1=last_commit+1
 sed -i "$last_commit_1",'$d' /tmp/patch
 cut -d ' ' -f 1 /tmp/patch > /tmp/commit_id
@@ -15,5 +18,9 @@ cut -d ' ' -f 1 /tmp/patch > /tmp/commit_id
 for line in `cat /tmp/commit_id`
 do
         git checkout $line
-        git format-patch -1
+        git format-patch -1 >> /tmp/patches
 done
+
+git checkout $BRANCH
+cp /tmp/patches ./
+rm /tmp/patch /tmp/commit_id /tmp/patches
