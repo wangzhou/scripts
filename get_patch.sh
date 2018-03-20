fe60eda3a1290e2ec312c33b11ff38dfbf877918
Sherlock Wang <wangzhou.bry@gmail.com>
2018-03-21 03:31:52 +0800

    fix get_patch.sh to delete Merge patch
    Signed-off-by: Sherlock Wang <wangzhou.bry@gmail.com>

--- get_patch.sh
+++ get_patch.sh
@@ -29,6 +29,7 @@ git log --oneline -- ./ > /tmp/patch
 last_commit=`grep "$1" -n /tmp/patch | cut -d : -f 1`
 let last_commit_1=last_commit+1
 sed -i "$last_commit_1",'$d' /tmp/patch
+sed -i '/Merge/d' /tmp/patch
 cut -d ' ' -f 1 /tmp/patch > /tmp/commit_id_r
 tac /tmp/commit_id_r > /tmp/commit_id
 
