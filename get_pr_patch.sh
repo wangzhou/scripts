643eb36e4c84a0d1ca44dde04e723084a43934b7
Zhou Wang <wangzhou1@hisilicon.com>
2021-01-12 23:41:30 +0800

    ...
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- get_pr_patch.sh
+++ get_pr_patch.sh
@@ -3,4 +3,7 @@
 #
 # $1 is the number of PR
 
-curl -L http://github.com/Linaro/uadk/pull/$1.patch > $1.patch
+# This will add all patches in one patch set to one file.
+#curl -L http://github.com/Linaro/uadk/pull/$1.patch > $1.patch
+
+git pull origin pull/24/head
