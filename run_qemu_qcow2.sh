8616f99359cd3c2c22a65c95d8a55030a9399a0d
Sherlock <sherlock@SherlockdeMacBook-Air.local>
2026-04-13 13:51:50 +0800

    ...
    Signed-off-by: Sherlock <sherlock@SherlockdeMacBook-Air.local>

--- run_qemu_qcow2.sh
+++ run_qemu_qcow2.sh
@@ -2,13 +2,16 @@
 
 qemu-system-aarch64 \
   -machine virt,gic-version=3 \
-  -cpu cortex-a72 \
-  -accel tcg,thread=multi \
-  -m 4G \
-  -smp 4 \
-  -drive file=openEuler-24.03-LTS-SP3-aarch64.qcow2,format=qcow2,if=virtio \
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -cpu host \
+  -accel hvf \
+  -m 16G \
+  -smp 10 \
+  -drive file=~/ISO/openEuler-24.03-LTS-SP2-aarch64.qcow2,format=qcow2,if=virtio \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22,hostfwd=tcp::4000-:4000\
   -device virtio-net-pci,netdev=net0 \
   -nographic \
-  -bios ~/repos/qemu/pc-bios/edk2-aarch64-code.fd
+  -bios /opt/homebrew/share/qemu/edk2-aarch64-code.fd \
+  -fsdev local,id=fsdev0,path=/Users/sherlock,security_model=none \
+  -device virtio-9p-pci,fsdev=fsdev0,mount_tag=hostshare \
+  -rtc clock=host
 #  -serial mon:stdio
